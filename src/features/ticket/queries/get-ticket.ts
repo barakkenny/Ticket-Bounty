38cdfdc30514d589ef32852870623a43eb38e4bc
Kehinde Yusuf <95845686+barakkenny@users.noreply.github.com>
2025-09-30 23:53:50 +0100

    Added prisma schema

--- src/features/ticket/queries/get-ticket.ts
+++ src/features/ticket/queries/get-ticket.ts
@@ -3,7 +3,7 @@ import { Ticket } from "../types"
 
 export const getTicket = async (ticketId: string): Promise<Ticket | null> => {
     await new Promise((resolve) => setTimeout(resolve, 2000))
-    throw new Error("failed to fetch")
+
     const maybeTicket = initialTickets.find((ticket) => ticket.id === ticketId)
     
     return new Promise((resolve) => {
